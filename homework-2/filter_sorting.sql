4d71ed53e44121782dcd420bf41c4f74a9ca60a0
okomarda <okomarda@yandex.ru>
2023-06-23 10:53:57 +0300

    Внесен изменения в файл filter_sorting.sql

--- homework-2/filter_sorting.sql
+++ homework-2/filter_sorting.sql
@@ -17,9 +17,9 @@ WHERE ship_country = 'Germany' AND shipped_date IS NOT NULL
 -- 4. минимальную и максимальную цену среди продуктов, не снятых с продажи (таблица products, колонки unit_price, discontinued не равно 1)
 SELECT MIN(unit_price), MAX(unit_price)
 FROM products
-WHERE discontinued = 0
+WHERE discontinued != 1
 
 -- 5. минимальную и максимальную цену среди продуктов, не снятых с продажи и которых имеется не меньше 20 (таблица products, колонки unit_price, units_in_stock, discontinued не равно 1)
 SELECT MIN(unit_price), MAX(unit_price)
 FROM products
-WHERE discontinued = 0 AND units_in_stock >= 20
+WHERE discontinued != 1 AND units_in_stock >= 20
